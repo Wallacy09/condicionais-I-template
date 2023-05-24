cc455a7d518fd88bda199e4f314412573f07c0c1
Wallacy <wallacy_vinicius@hotmail.com>
2023-05-24 19:06:01 -0300

    aula-condicionais1

---
+++ script.js
@@ -0,0 +1,67 @@
+// const booleano1 = true
+// const booleano2 = false
+// const booleano3 = true
+
+// if(condição){
+//     se a condição for true, o código será executado
+// }
+
+// if (booleano1){
+//     alert(`booleano1 é TRUE`)
+// }else{
+//     alert(`booleano1 é FALSE`)
+// }
+
+// if(booleano1 === booleano2){
+//     alert("booleano1 é igual a boleano2")
+
+// }else if(booleano2 === booleano3){
+//     alert("booleano2 é igual a booleano3")
+
+// }else if(booleano1 === booleano3){
+//     alert("booleano1 é igual a boleano3")
+
+// }else{
+//     alert("nenhum valor é igual a outro")
+
+// }
+
+// let idade = 17
+// let autorizacao = true
+
+// if(idade >= 18){
+//     alert("pode votar")
+
+// }else{
+//     if(idade === 16 || idade === 17 && autorizacao === true){
+//         alert("pode votar com a autorização dos pais")
+//     }else{
+//     alert("não pode votar")
+//     }
+// }
+
+// let idade = 79
+
+// if(idade >= 18){
+//     alert("pode votar")
+// }else if(idade === 16 || idade === 17){
+//     alert("pode votar com autorização dos pais")
+// }else if(idade >=70){
+//     alert("pode votar, mas não obrigatório")
+// }else{
+//     alert("não pode votar")
+// }
+
+
+
+// Exercícios
+const media = 6
+
+if(media >= 5){
+    console.log("Aprovado");
+}else if(media >= 3){
+    console.log("Recuperação");
+}else if(media < 3){
+    console.log("Estudante reprovado");
+}else
+    console.log("Dado inválido");
